a0e3b465891c2cc455392c7b11b9fda5c5a6cc36
zagortenay333 <krionubis@gmail.com>
2015-11-08 14:26:50 +0100

    update gulpfile.js

--- Ciliora-Secunda/gnome-shell/gulpfile.js
+++ Ciliora-Secunda/gnome-shell/gulpfile.js
@@ -1,67 +1,86 @@
-var gulp        = require('gulp'),
-    plumber     = require('gulp-plumber'),
-    notify      = require('gulp-notify'),
-    gutil       = require('gulp-util'),
-    beep        = require('beepbeep'),
-    shell       = require('gulp-shell'),
-    fs          = require('fs'),
-    sass        = require('gulp-sass');
+var gulp        = require("gulp"),
+    plumber     = require("gulp-plumber"),
+    notify      = require("gulp-notify"),
+    gutil       = require("gulp-util"),
+    beep        = require("beepbeep"),
+    shell       = require("gulp-shell"),
+    fs          = require("fs"),
+    rimraf      = require("rimraf"),
+    sass        = require("gulp-sass");
 
+
+var themesDir   = process.env.HOME+"/.themes/",
+    theme       = "Ciliora-Secunda";
 
 
 // Error handler
 var onError = function (err) {
-    var errorLine   = (err.line) ? 'Line ' + err.line : '',
-        errorTitle  = (err.plugin) ? 'Error: [ ' + err.plugin + ' ]' : 'Error';
+    var errorLine   = (err.line) ? "Line " + err.line : "",
+        errorTitle  = (err.plugin) ? "Error in plugin: [ " + err.plugin + " ]" : "Error";
 
     notify.logLevel(0);
     notify({
             title: errorTitle,
             message: errorLine
     }).write(err);
+
     beep();
-    gutil.log(gutil.colors.red('\n'+errorTitle+'\n\n', err.message));
-    this.emit('end');
+
+    gutil.log(gutil.colors.red("\n"+errorTitle+"\n\n", err.message));
+
+    this.emit("end");
 };
 
 
 // Compile sass
-gulp.task('sass', function () {
-    return gulp.src('sass/gnome-shell.scss')
+gulp.task("sass", function () {
+    return gulp.src("sass/gnome-shell.scss")
         .pipe(plumber({ errorHandler: onError }))
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(gulp.dest('.'));
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(gulp.dest("."));
 });
 
 
 // Wait for sass to compile & reload theme
-gulp.task('reloadTheme', ['sass'], shell.task([
-    'gsettings set org.gnome.shell.extensions.user-theme name default',
-    'gsettings set org.gnome.shell.extensions.user-theme name Ciliora-Secunda'
+gulp.task("reloadTheme", ["sass"], shell.task([
+    "gsettings set org.gnome.shell.extensions.user-theme name default",
+    "gsettings set org.gnome.shell.extensions.user-theme name "+theme
 ]));
 
-
-// Make zip backup (not used by default)
-gulp.task('backup', shell.task([
-    'zip -qr ~/Dropbox/secunda ../../../ciliora-secunda-shell -x "*node_modules*" "*.git*"'
-]));
 
 
 // Make a symlink in the ~/.themes dir
-gulp.task('install', function () {
+gulp.task("install", function () {
+
+    // Ensure that ".themes" dir exists
     try {
-        fs.mkdirSync(process.env.HOME+'/.themes');
+        fs.mkdirSync(themesDir);
     } catch (err) {
-        if (err.code !== 'EEXIST') throw err;
+        if (err.code !== "EEXIST") throw err;
     }
+
+
+    // Check whether the theme dir/file exists in ".themes"
+    // If it does exist, remove it
     try {
-        fs.unlinkSync(process.env.HOME+'/.themes/Ciliora-Secunda');
+        if( fs.lstatSync(themesDir+theme).isDirectory() ||
+            fs.lstatSync(themesDir+theme).isFile() ) {
+
+            rimraf.sync(themesDir+theme);
+        }
     } catch (err) {
-        if (err.code !== 'ENOENT') throw err;
+        if (err.code !== "ENOENT") throw err;
     }
-        fs.symlinkSync(__dirname+'/../../Ciliora-Secunda', process.env.HOME+'/.themes/Ciliora-Secunda');
+
+
+    // Create/update the link
+    try {
+        fs.unlinkSync(themesDir+theme);
+    } catch (err) {
+        if (err.code !== "ENOENT") throw err;
+    }
+
+    fs.symlinkSync(__dirname+"/../../"+theme, themesDir+theme);
 });
 
 
@@ -80,6 +99,6 @@ gulp.task('watch', function () {
 
 
 // Default task
-gulp.task('default', ['install', 'watch'], function() {
-    gulp.start('reloadTheme');
+gulp.task("default", ["install", "watch"], function() {
+    gulp.start("reloadTheme");
 });
